45e72d6208ae4c5371060c6f02a369b3a48e0338
ohgus <anakin1108@naver.com>
2023-12-16 15:44:24 +0900

    feat: 근무 순서 입력 예외처리 추가

--- src/constants/error.js
+++ src/constants/error.js
@@ -1,6 +1,25 @@
 const ERROR = Object.freeze({
   regex: {
     month: /^(1[0-2]|[1-9])$/,
+    name: /^[가-힣]+$/,
+  },
+  nameLength: 5,
+  people: {
+    max: 35,
+    min: 5,
+  },
+  message: {
+    name: {
+      kor: "[ERROR] 이름은 한글로만 입력해 주세요.",
+      length: "[ERROR] 이름은 1자 이상 5자 이하로 입력해 주세요.",
+    },
+    people: "[ERROR] 근무자의 수는 최소 5명 최대 35명 입니다.",
+    schedule: {
+      onceInWhile:
+        "[ERROR] 한명의 근무자는 평일, 휴일 스케쥴에 한번씩 포함되어야 합니다. 다시 입력해 주세요.",
+      duplicate:
+        "[ERROR] 하나의 근무 스케쥴에 중복된 이름은 사용할 수 없습니다. 다시 입력해 주세요.",
+    },
   },
 });
 
--- src/model/Schedule.js
+++ src/model/Schedule.js
@@ -1,3 +1,5 @@
+import ERROR from "../constants/error.js";
+
 class Schedule {
   #weekday;
   #dayoff;
@@ -10,8 +12,54 @@ class Schedule {
   setScheduleInput(input) {
     const [weekday, dayoff] = input;
 
-    this.#weekday = weekday.split(",");
-    this.#dayoff = dayoff.split(",");
+    this.#validate(weekday, dayoff);
+
+    this.#weekday = weekday;
+    this.#dayoff = dayoff;
+  }
+
+  #validate(weekday, dayoff) {
+    if (
+      weekday.length < ERROR.people.min ||
+      weekday.length > ERROR.people.max
+    ) {
+      throw new Error(ERROR.message.people);
+    } else if (
+      dayoff.length < ERROR.people.min ||
+      dayoff.length > ERROR.people.max
+    ) {
+      throw new Error(ERROR.message.people);
+    } else if (
+      weekday.length !== new Set(weekday).size ||
+      dayoff.length !== new Set(dayoff).size
+    ) {
+      throw new Error(ERROR.message.schedule.duplicate);
+    }
+
+    weekday.forEach((name) => {
+      if (!dayoff.includes(name)) {
+        throw new Error(ERROR.message.schedule.onceInWhile);
+      }
+    });
+
+    dayoff.forEach((name) => {
+      if (!weekday.includes(name)) {
+        throw new Error(ERROR.message.schedule.onceInWhile);
+      }
+    });
+
+    this.#validateName(weekday);
+    this.#validateName(dayoff);
+  }
+
+  #validateName(schedule) {
+    schedule.forEach((name) => {
+      if (!ERROR.regex.name.test(name)) {
+        throw new Error(ERROR.message.name.kor);
+      } else if (name.length > ERROR.nameLength || name.length < 1) {
+        throw new Error(ERROR.message.name.length);
+      }
+    });
   }
 }
 
