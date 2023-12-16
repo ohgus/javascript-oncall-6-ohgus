8006c3047e7c6b6fbcaba46ce8331f4d50cfa2d4
ohgus <anakin1108@naver.com>
2023-12-16 16:07:43 +0900

    feat: 달력 정보 세팅 기능 추가

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,5 +1,6 @@
 import Callender from "../model/Callender.js";
 import Schedule from "../model/Schedule.js";
+import InputView from "../view/InputView.js";
 
 class Controller {
   #callenderInfo;
@@ -10,7 +11,18 @@ class Controller {
     this.#scheduleInfo = new Schedule();
   }
 
-  async start() {}
+  async start() {
+    await this.#setCallender();
+  }
+
+  async #setCallender() {
+    try {
+      const input = await InputView.readDate();
+      this.#callenderInfo.setDateInput(input);
+    } catch (error) {
+      return await this.#setCallender();
+    }
+  }
 }
 
 export default Controller;
